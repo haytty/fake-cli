17111752b2a4d28f8cd8e71bc712eb8c9d9250d6
H.N <hayato.n.fl@gmail.com>
2024-07-23 15:53:00 +0900

    Fix count option.

--- src/cli.rs
+++ src/cli.rs
@@ -11,20 +11,40 @@ struct Args {
     json: String,
 
     #[arg(short, long, default_value_t = 1)]
-    count: u32,
+    count: usize,
 }
-
 
 pub fn start() -> Result<()> {
-    let _args = Args::parse();
-
-    let fake_definition_json = file::load_json(_args.json)?;
-    output_json(&fake_definition_json)
+    let args = Args::parse();
+    let fake_definition_json = file::load_json(args.json)?;
+    output_json(&fake_definition_json, args.count)
 }
 
-fn output_json(fake_definition_json: &Value) -> Result<()> {
+fn output_json(fake_definition_json: &Value, count: usize) -> Result<()> {
+    match count {
+        1 => output_single_fake_definition(fake_definition_json),
+        _ => output_multiply_fake_definition(fake_definition_json, count)
+    }
+}
+
+fn output_single_fake_definition(fake_definition_json: &Value) -> Result<()> {
     let fake_definition = FakeDefinition::from_json(fake_definition_json)?;
     let json = serde_json::to_string_pretty(&fake_definition.to_value())?;
+    println!("{}", json);
+    Ok(())
+}
+
+fn output_multiply_fake_definition(fake_definition_json: &Value, count: usize) -> Result<()> {
+    let fake_definitions = (1..=count)
+        .map(|_| FakeDefinition::from_json(fake_definition_json))
+        .collect::<Result<Vec<FakeDefinition>, _>>()?;
+
+    let values: Vec<_> = fake_definitions
+        .iter()
+        .map(|fake_definition| { fake_definition.to_value() })
+        .collect();
+
+    let json = serde_json::to_string_pretty(&values)?;
     println!("{}", json);
 
     Ok(())
